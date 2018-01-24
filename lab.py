22c8e8c92cd9113ede03538435fcb596d8324b42
wilsj878 <wllmsjblm@gmail.com>
2018-01-24 12:41:34 +0100

    lol xD

--- lab.py
+++ lab.py
@@ -1,14 +1,6 @@
-import cv2
-
 import vision
 
 def find_ball(image):
-    colorLower = (3, 100, 2)
-    colorUpper = (35, 255, 255)
     
-    image.blur(100)
-    #image.in_range(colorLower, colorUpper)
-    image.show()
 
-    
 vision.run(find_ball)
--- vision.py
+++ vision.py
@@ -62,18 +62,11 @@ def run(callback):
     cv2.namedWindow('Frame')
     
     while True:
-        # grab the current frame
-	(grabbed, frame) = camera.read()
+        (grabbed, frame) = camera.read()
         
-	# resize the frame, blur it, and convert it to the HSV
-        # color space
-	#frame = imutils.resize(frame, width=600)
         image = Image(frame)
-
         callback(image)
         
-	# # find contours in the mask and initialize the current
-	# # (x, y) center of the ball
         if image.grayscale:
 	    cnts = cv2.findContours(image.hsv.copy(), cv2.RETR_EXTERNAL,
                                     cv2.CHAIN_APPROX_SIMPLE)[-2]
@@ -82,20 +75,14 @@ def run(callback):
 	    center = None
         radius = 0
 
-	# only proceed if at least one contour was found
 	if len(cnts) > 0:
-	    # find the largest contour in the mask, then use
-	    # it to compute the minimum enclosing circle and
-	    # centroid
 	    c = max(cnts, key=cv2.contourArea)
 	    (x, y), radius = cv2.minEnclosingCircle(c)
 	    M = cv2.moments(c)
-	    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            if M["m00"] != 0:
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
             
-	    # only proceed if the radius meets a minimum size
 	    if radius > MIN_RADIUS:
-		# draw the circle and centroid on the frame,
-		# then update the list of tracked points
 		cv2.circle(frame, (int(x), int(y)), int(radius),
 			   (0, 255, 255), 2)
 		cv2.circle(frame, center, 5, (0, 0, 255), -1)
@@ -103,26 +90,18 @@ def run(callback):
         if radius > MIN_RADIUS:
             pts.appendleft(center)
 
-	# loop over the set of tracked points
 	for i in xrange(1, len(pts)):
-	    # if either of the tracked points are None,image.hsv ignore
-            # them
             if pts[i - 1] is None or pts[i] is None:
 		continue
 
-            # otherwise, compute the thickness of the line and
-            # draw the connecting lines
             thickness = int(np.sqrt(BUF_SZ / float(i + 1)) * 2.5)
 	    cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
-	# show the frame to our screen
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
         
-	# if the 'q' key is pressed, stop the loop
 	if key == ord("q"):
             break
     
-    # cleanup the camera and close any open windows
     camera.release()
     cv2.destroyAllWindows()
